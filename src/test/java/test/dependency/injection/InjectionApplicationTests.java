2191c9dcd6e6c2793d69127ad685d18dd6e0d61b
administrator <masoudider@yahoo.com>
2019-10-25 03:55:04 +0330

    initialize

---
+++ src/test/java/test/dependency/injection/InjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package test.dependency.injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
